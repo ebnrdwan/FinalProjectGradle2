1b90391d754f9483a1231ed4c68cfc026f5a605c
ebnrdwan <eng.abdoehab@gmail.com>
2017-06-13 07:35:35 +0200

    initial

--- app/src/main/java/com/udacity/gradle/builditbigger/jokeAsync.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/jokeAsync.java
@@ -3,13 +3,15 @@ package com.udacity.gradle.builditbigger;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.abdulrhman.myapplication.backend.myApi.MyApi;
-
 import com.example.android.andjoke.AndJokeActivity;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
@@ -32,11 +34,13 @@ import java.io.IOException;
             if(myApiService == null) {  // Only do this once
                 MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                         new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // - 10.0.2.2 is localhost's IP address in Android emulator
-                        // - turn off compression when running against local devappserver
-//                        .setRootUrl("http://192.168.1.100:8080/_ah/api/");
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/");
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                    @Override
+                    public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                        abstractGoogleClientRequest.setDisableGZipContent(true);
+                    }
+                });
 
                 // end options for devappserver
 
@@ -56,6 +60,7 @@ import java.io.IOException;
             Toast.makeText(mContext, result, Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(mContext, AndJokeActivity.class);
 
+            Log.d("LOGRESULT",result);
             intent.putExtra(AndJokeActivity.JOKE_KEY, result);
             mContext.startActivity(intent);
         }
