56b10280a9e9bb96fbb56d4a7490eea245c222ed
ebnrdwan <eng.abdoehab@gmail.com>
2017-06-13 07:54:45 +0200

    initial

--- app/src/paid/java/MainActivityFragment.java
+++ app/src/paid/java/MainActivityFragment.java
@@ -5,8 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
+
 import com.udacity.gradle.builditbigger.R;
 import com.udacity.gradle.builditbigger.jokeAsync;
 
