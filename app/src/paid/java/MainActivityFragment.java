c26e59569ab90e33e10f40916f66d48f2b037886
ebnrdwan <eng.abdoehab@gmail.com>
2017-06-13 07:52:54 +0200

    initial

---
+++ app/src/paid/java/MainActivityFragment.java
@@ -0,0 +1,40 @@
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.builditbigger.R;
+import com.udacity.gradle.builditbigger.jokeAsync;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button button = (Button) root.findViewById(R.id.jokebutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                jokeAsync myjokeAsync = new jokeAsync(getActivity());
+                myjokeAsync.execute();
+            }
+        });
+
+
+        return root;
+    }
+}
